d77bdba0a072a752e4345edc99083eb4450ce233
Rio Oktaviansyah <riooktaviansyah36@gmail.com>
2024-06-07 08:36:42 +0700

    update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'rioovs.github.io',
+  site: 'https://rioovs.github.io',
   // base: 'my-repo',
   integrations: [sitemap(), robotsTxt({
     sitemap: ["rioovs.github.io/sitemap-index.xml", "rioovs.github.io/sitemap-0.xml"]
