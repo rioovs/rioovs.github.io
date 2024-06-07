722d14b54c68c4248b20c1c921aaee524acff96c
Rio Oktaviansyah <riooktaviansyah36@gmail.com>
2024-06-07 09:11:36 +0700

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
 import solidJs from "@astrojs/solid-js";
-import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
+import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
 import svelte from "@astrojs/svelte";
 
 import vercel from "@astrojs/vercel/serverless";
